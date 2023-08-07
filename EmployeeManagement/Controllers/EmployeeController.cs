0e16897311e9a0ac02825facab17764243f63e6e
tranhoangdong <dongtran100298@gmail.com>
2023-08-07 19:34:36 +0700

    Edit+delete

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -9,10 +9,12 @@ namespace EmployeeManagement.Controllers
     public class EmployeeController : Controller
     {
         // GET: Employee
-        public ActionResult Index()
+        public ActionResult Index(string search="")
         {
             EmployeeManagementEntities1 db = new EmployeeManagementEntities1();
-            List<Employee> Employees = db.Employees.ToList();
+            // List<Employee> Employees = db.Employees.ToList();
+            List<Employee> Employees = db.Employees.Where(row => row.LastName.Contains(search)).ToList();
+            ViewBag.Search = search;
             return View(Employees);
         }
         public ActionResult Create()
@@ -27,9 +29,51 @@ namespace EmployeeManagement.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
-        //public ActionResult Edit(int Id)
-        //{
+        public ActionResult Detail(int id)
+        {
+            EmployeeManagementEntities1 db = new EmployeeManagementEntities1();
+            Employee Emp = db.Employees.Where(row => row.Id == id).FirstOrDefault();
+            return View(Emp);
 
-        //}
+        }
+        public ActionResult Edit(int id)
+        {
+            EmployeeManagementEntities1 db = new EmployeeManagementEntities1();
+            Employee employee = db.Employees.Where(row => row.Id == id).FirstOrDefault();
+            return View(employee);
+
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee Emp)
+        {
+            EmployeeManagementEntities1 db = new EmployeeManagementEntities1();
+            Employee employee = db.Employees.Where(row => row.Id == Emp.Id).FirstOrDefault();
+
+            //update
+            employee.FirstName = Emp.FirstName;
+            employee.LastName = Emp.LastName;
+            employee.Address = Emp.Address;
+            employee.BirthPlace = Emp.BirthPlace;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult Delete(int id)
+        {
+            EmployeeManagementEntities1 db = new EmployeeManagementEntities1();
+            Employee employee = db.Employees.Where(row => row.Id == id).FirstOrDefault();
+            return View(employee);
+
+        }
+        [HttpPost]
+        public ActionResult Delete(int id,Employee p)
+        {
+            EmployeeManagementEntities1 db = new EmployeeManagementEntities1();
+            Employee employee = db.Employees.Where(row => row.Id == id).FirstOrDefault();
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index ");
+
+        }
     }
 }
